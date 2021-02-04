a015b8a229b429b54e505ab4b7eadbf5ffe1199f
Mikhail Kozhevnikov <kozhevnikov@metait.de>
2021-02-04 23:03:13 +0300

    second task.

--- 02-javascript-data-types/2-pick/index.js
+++ 02-javascript-data-types/2-pick/index.js
@@ -5,5 +5,13 @@
  * @returns {object} - returns the new object
  */
 export const pick = (obj, ...fields) => {
+  const result = {};
 
+  for (const key of Object.keys(obj)) {
+    if (fields.includes(key)) {
+      result[key] = obj[key];
+    }
+  }
+
+  return result;
 };
